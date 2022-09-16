0463497fb5c24014b6f9a5f08520ae90fcae48c7
vitali <xudyakovvb@email.com>
2022-09-16 17:25:22 +0700

    002

--- Firstapp/FirstApp2/Program.cs
+++ Firstapp/FirstApp2/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите квадрат первого числа");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(b == a * a)
+{
+    Console.WriteLine("ответ правельный");
+}
+else
+{
+    Console.WriteLine("ответ не верный");
+}
